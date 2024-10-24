344786c99d3b0af34c2c172b561dd3d7f8fa1eea
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-10-24 19:57:22 -0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
-const sass = require('gulp-sass')(require('sass'));
+const less = require('gulp-less');
+
+
 
 // Minify JavaScript
 gulp.task('minify-js', function() {
@@ -20,14 +22,12 @@ gulp.task('minify-images', async function() {
         .pipe(gulp.dest('dist/images'));
 });
 
-// Compile SCSS and minify CSS
-gulp.task('scss', function() {
-    return gulp.src('src/sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
+// Compile LESS files and minify
+gulp.task('less', function() {
+    return gulp.src('src/less/*.less')
+        .pipe(less())
         .pipe(cleanCSS())
-        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/css'));
 });
-
 // Default task
-gulp.task('default', gulp.series('minify-js', 'minify-images', 'scss'));
+gulp.task('default', gulp.series('minify-js', 'minify-images', 'less'));
